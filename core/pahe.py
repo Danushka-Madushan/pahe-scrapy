1db129026d248b2d93821a881f9c60598a86f090
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-04-14 13:47:24 +0530

    episode session endpoint

--- core/pahe.py
+++ core/pahe.py
@@ -6,7 +6,12 @@ class AnimePahe:
     def __init__(self) -> None:
         pass
 
-    def getAnimeList(query: str) -> str:
+    def getAnimeList(query: str) -> dict:
         params = { 'm': 'search', 'q': query }
         response = requests.get(ENV.API, params=params, headers=getHeaders())
-        return { "status": response.status_code, "content": response.json()}
+        return { "status": response.status_code, "content": response.json() if response.status_code == 200 else {}}
+
+    def getEpisodes(session: str) -> dict:
+        params = { 'm': 'release', 'id': session, 'sort': 'episode_asc', 'page': '1' }
+        response = requests.get('https://animepahe.com/api', params=params, headers=getHeaders())
+        return { "status": response.status_code, "content": response.json() if response.status_code == 200 else {}}
