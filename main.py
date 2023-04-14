ab7b49a79d8b1364e7433cdd8f64e868a390263a
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-04-14 10:37:06 +0530

    fastAPI server

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-# Main Python File Goes Here
+from fastapi import FastAPI
+from core.pahe import AnimePahe
+
+app = FastAPI()
+
+@app.get("/search/{query}")
+async def read_item(query: str) -> dict:
+    return AnimePahe.getAnimeList(query)
