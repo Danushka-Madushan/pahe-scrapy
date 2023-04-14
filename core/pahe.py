a8f81f77e8ff949905b4888b084b374bf3bf0238
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-04-14 19:22:19 +0530

    request episodes

--- core/pahe.py
+++ core/pahe.py
@@ -1,6 +1,8 @@
 # All Functions Goes Here
 import requests
-from .systems import getHeaders, ENV
+import re
+from html import unescape
+from .systems import getMaxRes, getHeaders, ENV
 
 class AnimePahe:
     def __init__(self) -> None:
@@ -9,9 +11,21 @@ class AnimePahe:
     def getAnimeList(query: str) -> dict:
         params = { 'm': 'search', 'q': query }
         response = requests.get(ENV.API, params=params, headers=getHeaders())
-        return { "status": response.status_code, "content": response.json() if response.status_code == 200 else {}}
+        return { "status": response.status_code, "content": response.json() if response.status_code == 200 else {} }
 
     def getEpisodes(session: str) -> dict:
         params = { 'm': 'release', 'id': session, 'sort': 'episode_asc', 'page': '1' }
-        response = requests.get('https://animepahe.com/api', params=params, headers=getHeaders())
-        return { "status": response.status_code, "content": response.json() if response.status_code == 200 else {}}
+        response = requests.get(ENV.API, params=params, headers=getHeaders())
+        return { "status": response.status_code, "content": response.json() if response.status_code == 200 else {} }
+    
+    def getEpisode(session: str, playid: str) -> dict:
+        response = requests.get(ENV.PLAY_API + session + '/' + playid)
+        matches = re.findall(r'<a.href="(https?://pahe.win/[^"]+)".target="_blank"[^>]+.(.+?)<', response.content.decode('utf-8'))
+        print(matches)
+        if (len(matches) != 0):
+            linkset = getMaxRes(matches)
+            resp = requests.get(linkset[0])
+            mat = re.findall(r',"(https?://kwik.cx/f/[^"]+)', resp.content.decode('utf-8'))
+            return { "status": response.status_code, "content": { "link": mat[0], "info": unescape(linkset[1])} }
+        else:
+            return { "status": 500, "content": {} }
